13a6558b0d458e6545ad2456c93e7276ce368bac
Pooria Abbasi <pouria_abbasi@hotmail.com>
2022-07-19 18:32:43 +0430

    change MongoDB URL

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-// const dbConnection = "mongodb://localhost:27017/MovieCollection";
-const dbConnection = "mongodb+srv://dev:Vc2mHf9y2pIIaw1U@cluster0.7riz9mc.mongodb.net/?retryWrites=true&w=majority";
+const dbConnection = "mongodb://localhost:27017/MovieCollection";
+// const dbConnection = "mongodb+srv://dev:Vc2mHf9y2pIIaw1U@cluster0.7riz9mc.mongodb.net/?retryWrites=true&w=majority";
 
 const initDatabaseConnection = () => {
-    mongoose.connect(dbConnection)
+    mongoose.connect(dbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Now connected to MongoDB!'))
         .catch(err => console.error('Something went wrong', err));
 }
