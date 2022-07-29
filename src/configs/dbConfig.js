01514f87c86c57de69ad82f1b6403d48e88c8da4
Pooria Abbasi <pouria_abbasi@hotmail.com>
2022-07-30 00:20:51 +0430

    change db connection

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const dbConnection = "mongodb://localhost:27017/MovieCollection";
-// const dbConnection = "mongodb+srv://dev:Vc2mHf9y2pIIaw1U@cluster0.7riz9mc.mongodb.net/?retryWrites=true&w=majority";
+// const dbConnection = "mongodb://localhost:27017/MovieCollection";
+const dbConnection = "mongodb+srv://dev:Vc2mHf9y2pIIaw1U@cluster0.7riz9mc.mongodb.net/?retryWrites=true&w=majority";
 
 const initDatabaseConnection = () => {
     mongoose.connect(dbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
